7d71f9dddb9d708bfb7df8294d8b4221457cfcbf
mValentinaDev <70847744+mValentinaDev@users.noreply.github.com>
2022-04-14 18:01:31 +0300

    auth complete

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,8 +11,8 @@ import PostMessage from '../models/postMessage.js';
 };
 
 export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
-    const newPost = new PostMessage({ title, message, selectedFile, creator, tags });
+    const post = req.body;
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Data().toISOString() });
     try {
         await newPost.save();
         res.status(201).json(newPost);
@@ -44,8 +44,19 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
+    if(!req.userId) return res.json({ message: 'Unauthenticated' });
+
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with this id');
     const post = await PostMessage.findById(id);
-    const updatePost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true });
-    res.json(updatePost);
+
+    const index = post.likes.findById((id) => id === String(req.userId));
+    if(index === -1) {
+        //like post
+        post.likes.push(req.userId);
+    } else {
+        // dislike post
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+    res.json(updatedPost);
 }
