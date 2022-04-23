fb58eb5a9e04ac42d9899969e3b456c9b6089d68
mValentinaDev <70847744+mValentinaDev@users.noreply.github.com>
2022-04-23 16:42:21 +0300

    working prototype

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,28 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+
 const secret = 'test';
 
 const auth = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const isCustomAuth = token.length < 500;
-        let decodedData;
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
 
-        if(token && isCustomAuth) {
-            decodedData = jwt.verify(token, secret);
-            //req.userId = decodedData?.id;
-            req.userId = decodedData.id;
-        } else {
-            decodedData = jwt.decode(token);
-            //req.userId = decodedData?.sub;
-            req.userId = decodedData.sub;
-        }
+    let decodedData;
 
-        next();
-    } catch (error) {
-        console.log(error);
-    }
+    if (token && isCustomAuth) {      
+      decodedData = jwt.verify(token, secret);
+
+      req.userId = decodedData.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      req.userId = decodedData.sub;
+    }    
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default auth;
--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,17 @@
 import express from 'express';
 
-import { getPosts, getPostsBySearch, createPost, updatePost, deletePost, likePost  } from '../controllers/posts.js';
-import auth from '../middleware/auth.js';
+import { getPosts, getPostsBySearch, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
-router.get('/', getPosts);
 router.get('/search', getPostsBySearch);
-router.post('/', auth, createPost);
+router.get('/', getPosts);
+router.get('/:id', getPost);
+
+router.post('/', auth,  createPost);
 router.patch('/:id', auth, updatePost);
-router.delete(':id', auth, deletePost);
+router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
 
 export default router;
