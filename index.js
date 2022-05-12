1c0a1d8f563a955713e826c8be63434000648ddc
mValentinaDev <70847744+mValentinaDev@users.noreply.github.com>
2022-05-12 15:44:57 +0300

    cors bug fix

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ import userRouter from './routes/user.js';
 import cors from 'cors';
 
 const app = express();
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': '*',
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 
 dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors());
 
 app.use('/jobs', jobRoutes);
 app.use('/user', userRouter);
