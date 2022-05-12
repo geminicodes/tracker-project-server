0626dd432ff366224f3f44cd5f57add1bfe877b6
mValentinaDev <70847744+mValentinaDev@users.noreply.github.com>
2022-05-12 15:56:44 +0300

    project complete

--- index.js
+++ index.js
@@ -10,21 +10,15 @@ const app = express();
 
 dotenv.config();
 
-app.use(express.json({ limit: '30mb', extended: true }));
-app.use(express.urlencoded({ limit: '30mb', extended: true }));
-app.use(cors({
-  allowedHeaders: ['sessionId', 'Content-Type'],
-  exposedHeaders: ['sessionId'],
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false
-}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
 app.use('/jobs', jobRoutes);
 app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
-  res.send('APP IS RUNNING')
+  res.send('Hello, World!');
 })
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
