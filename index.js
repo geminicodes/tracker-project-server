d62a0e29d8a91b389ecf773ca1df158bd1909a3d
mValentinaDev <70847744+mValentinaDev@users.noreply.github.com>
2022-05-12 15:18:07 +0300

    minor bug fix

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors({ origin: "*", credentials: true }));
-
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
 app.use('/jobs', jobRoutes);
 app.use('/user', userRouter);
